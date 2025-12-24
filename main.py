6308be5ad6d722c6790e2593a2511d55452e6828
sametucuncu <sametucuncu61@gmail.com>
2025-12-24 20:03:54 +0000

    adding fps

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
 
-
+        
+        # End of each iteration of the game loop
+        # It will pause the game loop until 1/60th of a second has passed.
+        dt = clock.tick(60) / 1000  # convert from miliseconds to seconds.
+        print(dt)
 if __name__ == "__main__":
     main()
